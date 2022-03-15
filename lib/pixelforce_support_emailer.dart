d14a764409faa9ded28905bc407496cd3caa96ac
Ben Zhang <bzbnhang@gmail.com>
2022-03-15 12:16:46 +1030

    add new methods

--- lib/pixelforce_support_emailer.dart
+++ lib/pixelforce_support_emailer.dart
@@ -1,5 +1,13 @@
 library pixelforce_support_emailer;
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:open_mail_app/open_mail_app.dart';
 
 class PixelSupportMailer {
-  int userId?;
+  String? userId;
+
+  PixelSupportMailer(this.userId);
+
+  void sendSupportEmail(String toEmailAddress, String subject) {
+
+  }
 }
--- test/pixelforce_support_emailer_test.dart
+++ test/pixelforce_support_emailer_test.dart
@@ -3,10 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pixelforce_support_emailer/pixelforce_support_emailer.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('should load Email', () {
+    final supportEmailer = PixelSupportMailer('33');
+    supportEmailer.sendSupportEmail('test@test.com');
   });
 }
